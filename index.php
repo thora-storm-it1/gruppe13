5ec903a01e1222bad9aceca53c28f4dc2665c628
Eirike01 <eirikeggset@hotmail.no>
2019-03-05 09:01:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,45 @@
 <head>
 	<title> Prosjekt 2019 </title>
 	<meta charset="UTF-8">
-</head>
+	<link rel="stylesheet" href="stilark/stilark.css">
+	</head>
+
 
 <body>
 
-aasd
+	<div class="navigasjon">
+		<div class="navigasjon1"> </div>
+		<div class="navigasjon2"> </div>
 
-</body>
+		</div>
+
+	</body>
+
+
+
+<style>
+	body{
+		font-family:Century Gothic;
+	}
+
+	.navigasjon{
+		display:flex;
+		flex-flow:row wrap;
+	}
+
+	.navigasjon1{
+		width:100%;
+		background-color:#2D3AB4;
+	}
+
+	.navigasjon2{
+		width:100%;
+		background-color:#F51717;
+	}
+
+
+</style>
+
+
 
 </html>
