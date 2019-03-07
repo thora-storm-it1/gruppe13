f15e5622922bc1b68368298776357e04f12c0cc7
Eirike01 <eirikeggset@hotmail.no>
2019-03-07 09:17:10 +0100

    Update meny.php

--- meny.php
+++ meny.php
@@ -1,26 +1,27 @@
-<html>
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="stilark/stilark.css">
-</head>
-
-<body>
 	<div class="tittel"> Yeet </div>
 		<div class="navigering">
 			<?php
 
 			?>
-			<!--
-			<a href="#" class="aktiv"> Hjem </a>
-			<a href="#"> Lenke 1 </a>
-			<a href="#"> Lenke 2 </a>
-			<a href="#"> Lenke 3 </a>
-			<a href="#"> Lenke 4 </a>
-			-->
+			<div class="dropdown">
+				<button class="dropdownknapp"> <a href="#"> Knapp 1 </a> </button>
+				<div class="dropdowninnhold">
+					<a href="#"> Lenke 1 </a>
+					<a href="#"> Lenke 2 </a>
+					<a href="#"> Lenke 3 </a>
+					<a href="#"> Lenke 4 </a>
+				</div>
+
+
+			</div>
 		</div>
-</body>
 
 
 <style>
-	.dropdown{width: 100%; display: inline-block; position: relative;}
-	.dropdownknapp{background-color: red; padding: 16px;}
+	.dropdown{ display: inline-block; position: relative;}
+	.dropdownknapp{ background-color: red; padding: 20px; text-decoration: none; color:black; font-size: 17px;}
+	.dropdowninnhold{ display:none; position:absolute; }
+	.dropdowninnhold a{  color: black; ; text-decoration: none; display: block; padding:20px; }
+	.dropdown:hover .dropdowninnhold{ display:block; background-color:yellow; }
+</style>
+
