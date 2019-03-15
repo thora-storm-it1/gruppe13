7ed5175f3642b9d7e4743dc8db62a408992c2968
Eirike01 <eirikeggset@hotmail.no>
2019-03-15 12:36:08 +0100

    Update vare.php

--- vare.php
+++ vare.php
@@ -8,59 +8,60 @@
 
 <body>
 
-<?php
+
+<! Vareinformasjon ->
+
+	<?php
 
 
 
-	if(isset($_GET["vare_id"])){
-		$vare_id = $_GET["vare_id"];
-	}
-	else{
-		die("Du må velge en vare.");
-	}
-	
-	$tjener = "localhost";
-	$brukernavn = "root";
-	$passord = "";
-	$database = "prosjekt2019";
+		if(isset($_GET["vare_id"])){
+			$vare_id = $_GET["vare_id"];
+		}
+		else{
+			die("Du må velge en vare.");
+		}
+		
+		$tjener = "localhost";
+		$brukernavn = "root";
+		$passord = "";
+		$database = "prosjekt2019";
 
-	$kobling = new mysqli($tjener, $brukernavn, $passord, $database);
-
-
-	if ($kobling->connect_error) {
-		die("Noe gikk galt: " . $kobling->connect_error);
-	}
-
-	$kobling->set_charset("utf8");
+		$kobling = new mysqli($tjener, $brukernavn, $passord, $database);
 
 
-	$sql="SELECT * FROM kategori JOIN vare ON vare.kategori_id=kategori.kategori_id WHERE vare_id=$vare_id";
-	$resultat = $kobling->query($sql);
+		if ($kobling->connect_error) {
+			die("Noe gikk galt: " . $kobling->connect_error);
+		}
 
-	while($rad = $resultat->fetch_assoc()){
-			
-			$varenavn = $rad["varenavn"];
-			$kategorinavn = $rad["kategorinavn"];
-			$rating = $rad["rating"];
-			$pris = $rad["pris"];
-			$bildeurl = $rad["bildeurl"];
-			$beskrivelse = $rad["beskrivelse"];
+		$kobling->set_charset("utf8");
 
 
-			/* Sidekode */
+		$sql="SELECT * FROM kategori JOIN vare ON vare.kategori_id=kategori.kategori_id WHERE vare_id=$vare_id";
+		$resultat = $kobling->query($sql);
 
-			echo "
-					<h1> $varenavn </h1> <br>
-					<img src='$bildeurl' width=150px>
-					<p> $beskrivelse <p>
-					The $varenavn goes for $$pris <br>
-					We have given the $varenavn a rating of $rating/10
-					";
-	
-	}
+		while($rad = $resultat->fetch_assoc()){
+				
+				$varenavn = $rad["varenavn"];
+				$kategorinavn = $rad["kategorinavn"];
+				$rating = $rad["rating"];
+				$pris = $rad["pris"];
+				$bildeurl = $rad["bildeurl"];
+				$beskrivelse = $rad["beskrivelse"];
 
 
-?>
+				echo "
+						<h1> $varenavn </h1> <br>
+						<img src='$bildeurl' width=150px>
+						<p> $beskrivelse <p>
+						The $varenavn goes for $$pris <br>
+						We have given the $varenavn a rating of $rating/10
+						";
+		
+		}
+
+
+	?>
 
 
 <h2> Kommentarfelt </h2>
@@ -135,13 +136,16 @@
 
 			$sql_3 = "INSERT INTO kommentar (kommentartekst, kommentar.vare_id, kommentar.bruker_id) VALUES ('$kommentartekst', '$vare_id', '$bruker_id')";
 
+			
 
 			if($kobling->query($sql_3)) {
 			header("Refresh:0");
 			}
 
-			
 			}
+
+
+
 		}		
 			
 	?>
