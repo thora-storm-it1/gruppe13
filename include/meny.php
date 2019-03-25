8cfac8920cc9d247540dc9fd1f04e0c505dd1f1f
Eirike01 <eirikeggset@hotmail.no>
2019-03-25 10:51:46 +0100

    Darkmodetoggle redirect

--- include/meny.php
+++ include/meny.php
@@ -4,13 +4,17 @@
 	<title> Prosjekt 2019 </title>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="stilark/meny.css">
+	<?php
+		include "darkmode.php";
+	?>
+
 </head>
 
 
 <body>
 	
 	<div class="top">
-		<div class="tittel"> <a href="index.php">Yeet</a> </div>
+		<div class="tittel"> <?php echo "<a href='index.php?darkmode=$darkmode'> "?>Yeet</a> </div>
 		<div class="navigering">
 			<?php
 				echo"
@@ -29,7 +33,7 @@
 				<div class='dropdown'>
 					<div class='lenke'> <a href='#''> Others </a> </div>
 					<div class='dropdowninnhold'>
-						<a href='#''> Lenke 1 </a>
+						<a href='darkmodetoggle.php?darkmode=$darkmode&url=$url'> Toggle darkmode </a>
 						<a href='#''> Lenke 2 </a>
 					</div>
 				</div>"
