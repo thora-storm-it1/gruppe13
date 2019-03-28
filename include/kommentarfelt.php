de02ef0915a452148b4e4e7e351ee41e051df32f
Eirike01 <eirikeggset@hotmail.no>
2019-03-28 11:45:03 +0100

    Kommentarfelt

--- include/kommentarfelt.php
+++ include/kommentarfelt.php
@@ -1,46 +1,25 @@
+<head>
+	<link rel="stylesheet" href="stilark/kommentarfelt.css">
+</head>
+
 <body>
-	<h2> Comments </h2>
-
-	<?php
-
-
-		$sql = "SELECT * FROM kommentar JOIN bruker ON bruker.bruker_id=kommentar.bruker_id JOIN vare ON vare.vare_id=kommentar.vare_id WHERE kommentar.vare_id=$vare_id";
-
-		$resultat1 = $kobling->query($sql);
-
-		echo "<table> <tr> <td> Username </td> <td> </td> <td> Published </td> </tr>";
-
-		while($rad = $resultat1->fetch_assoc()){
-
-			$brukernavn = $rad["brukernavn"];
-			$kommentartekst = $rad["kommentartekst"];
-			$tid1 = date_create($rad["tid"]);
-			$dato = date_format($tid1, 'd.m.y');
-			$tid = date_format($tid1, 'H:i');
-
-			echo "<tr>
-					<td>$brukernavn</td>
-					<td>$kommentartekst</td>
-					<td>$dato at $tid</td>
-					</tr>";
-		}
-
-		echo "</table>";
-
-	
-
-
-	?>
 
 <h3> Leave a comment </h3>
 
 	<form method="POST">
-		Brukernavn
-		<input type="text" name="brukernavn"> <br>
-		Kommentar
-		<input type="text" name="kommentartekst"> <br>
 
-		<input type="submit" name="leggtil" value="Kommenter">
+		<table class="kommenter">
+			<tr>
+				<td>Brukernavn</td>
+				<td><input type="text" name="brukernavn" class="brukernavn"></td>
+			</tr>
+			<tr>
+				<td>Kommentar</td>
+				<td><textarea rows="5" cols="80" name="kommentartekst"></textarea></td>
+			</tr>
+		</table>
+
+		<input type="submit" name="leggtil" value="Comment">
 
 
 
@@ -89,6 +68,38 @@
 	
 
 	</form>
+
+<h2> Comments </h2>
+
+	<?php
+
+
+		$sql = "SELECT * FROM kommentar JOIN bruker ON bruker.bruker_id=kommentar.bruker_id JOIN vare ON vare.vare_id=kommentar.vare_id WHERE kommentar.vare_id=$vare_id";
+
+		$resultat1 = $kobling->query($sql);
+
+
+		while($rad = $resultat1->fetch_assoc()){
+
+			$brukernavn = $rad["brukernavn"];
+			$kommentartekst = $rad["kommentartekst"];
+			$tid1 = date_create($rad["tid"]);
+			$dato = date_format($tid1, 'd.m.y');
+			$tid = date_format($tid1, 'H:i');
+
+			echo 
+				"<div class='kommentar'>
+					<div class='kommentarinfo'>
+						<div> $brukernavn </div>
+						<div> Posted $dato at $tid </div> 
+					</div> 
+					<div class='kommentartekst'> $kommentartekst </div>
+				</div><br>";
+		}
+
+	?>
+
+
 
 
 </body>
