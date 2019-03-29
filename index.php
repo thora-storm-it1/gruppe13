649c337b672c9039a5429ccb530b3f8f75743edb
Eirik Myrhaug <emyrhaug@gmail.com>
2019-03-29 13:01:31 +0100

    margin: 0;

--- index.php
+++ index.php
@@ -24,33 +24,4 @@
 
 </body>
 
-
-
-<style>
-	body{
-		font-family: Courier New;
-		margin:0;
-	}
-
-	.navigasjon{
-		display:flex;
-		flex-flow:row wrap;
-		position:fixed;
-		height:30%;
-		width:100%;
-	}
-
-	.navigasjon1{
-		width:100%;
-		background-color:#2D3AB4;
-	}
-
-	.navigasjon2{
-		width:100%;
-		background-color:#F51717;
-	}
-
-</style>
-
-
 </html>
