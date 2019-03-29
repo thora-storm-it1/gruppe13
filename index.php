9b2156a7b3b8da04e93c62d971d7d63e7ef37a45
Eirike01 <eirikeggset@hotmail.no>
2019-03-29 17:10:03 +0100

    Preview på index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <head>
 	<title> Prosjekt 2019 </title>
+	<link rel="stylesheet" href="stilark/index.css">
 	<meta charset="UTF-8">
 	<?php 
 		$url = "index.php?a=1"
@@ -13,15 +14,53 @@
 
 <body>
 
+<?php
+	include "include/kobling.php";
+	include "include/darkmode.php";	
+	include "include/meny.php";
+?>
+
+<div class="filler" style="height:20%;"> </div>
+
+<div class="innhold">
+
+	<h3> Black marked wares for all your needs </h3>
+
+	<div class="preview">
 		<?php
-			include "include/darkmode.php";	
-			include "include/meny.php";
-			?>
+			$sql = "SELECT * FROM vare ORDER BY RAND() LIMIT 3";
+
+				$resultat = $kobling->query($sql);
+
+				while($rad = $resultat->fetch_assoc()){		
+					$varenavn = $rad["varenavn"];
+					$bildeurl = $rad["bildeurl"];
+					$rating = $rad["rating"];
+					$vare_id = $rad["vare_id"];
+					$pris = $rad["pris"];
+
+					echo "
+						<div class='vare'>
+							<div class='varebilde'>
+								<a href='vare.php?vare_id=$vare_id&darkmode=$darkmode'><img src='$bildeurl' width=100px> </a>
+							</div>
+
+							<div class='tekstlenke'>
+								<a href='vare.php?vare_id=$vare_id&darkmode=$darkmode'>$varenavn </a>
+							</div>
+						</div>";
+				}
 
 
-	<div class="innhold" style="height:50000px;">
+		?>
+
 	</div>
 
+
+</div>
+
 </body>
+
+<style>
 
 </html>
